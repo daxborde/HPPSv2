48e4dbb80910f1bdb5e36b124094f2d7af1b06f1
Dax Borde <daxborde+github@gmail.com>
2019-12-28 21:12:59 -0500

    python changes

--- app/renderer/components/LoggedIn.js
+++ app/renderer/components/LoggedIn.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { PythonShell } from 'python-shell';
 import PropTypes from 'prop-types';
-import runMainPy from '../../python/pyrun';
+const app = require('electron').remote.app
+const path = require('path')
+// import runMainPy from '../../python/pyrun';
 
 export default class LoggedIn extends Component {
   static propTypes = {
@@ -17,6 +20,29 @@ export default class LoggedIn extends Component {
   };
 
   componentDidMount() {
+    const runMainPy = () => {
+      console.log("madeit!");
+      // var python = require('child_process').spawn('python', ['./main.py']);
+      // python.stdout.on('data',function(data){
+      //     console.log("data: ",data.toString('utf8'));
+      // });
+
+      const mypath = path.join(app.getAppPath(), 'python/');
+
+      console.warn("path:"+mypath);
+
+      const options = {
+        mode: 'text',
+        scriptPath: mypath,
+      };
+      // path.join(app.getAppPath(), 'python/main.py')
+      PythonShell.run('main.py', options, (err, results) => {
+        if (err) throw err;
+        console.log('main.py finished.');
+        console.log('results', results);
+      });
+    };
+
     console.log("mount!");
     if (this.props.pythonStatus) {
       console.log("status!");

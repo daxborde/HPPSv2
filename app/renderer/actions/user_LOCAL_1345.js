64e14b76691a50b942a9c80b5612f878944de305
Dax <daxborde+github@gmail.com>
2020-02-24 22:29:45 -0500

    delete vscode backup files

--- app/renderer/actions/user_LOCAL_1345.js
+++
@@ -1,7 +0,0 @@
-// import { createAction } from 'redux-actions';
-
-// export default {
-//   login: createAction('USER_LOGIN'),
-//   logout: createAction('USER_LOGOUT'),
-//   startPy: createAction('START_PYTHON'),
-// };
--- app/renderer/actions/user_REMOTE_1138.js
+++
@@ -1,8 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export default {
-  login: createAction('USER_LOGIN'),
-  logout: createAction('USER_LOGOUT'),
-  startSql: createAction('START_SQL'),
-  stopSql: createAction('STOP_SQL'),
-};
